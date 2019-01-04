2b9a2125aeafc2718ccfb3a6ad91b11ec608b55a
Junaid Siddiqui <junaid.wim@gmail.com>
2019-01-04 18:56:28 -0500

    Created basic layout for weather cards, and implemented animated weather icon

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import ReactAnimatedWeather from 'react-animated-weather'
+
+const DEFAULTS = {
+  color: 'goldenrod',
+  size: 128,
+  animate: true
+};
+
+const setIcon = (weather) => {
+  if (weather != undefined) {
+    switch(weather){
+      case "clear-night":
+        return "CLEAR_NIGHT"
+      case "clear-day":
+        return "CLEAR_DAY"
+      case "partly-cloudy-night":
+        return "PARTLY_CLOUDY_NIGHT"
+      case "partly-cloudy-day":
+        return "PARTLY_CLOUDY_DAY"
+      default:
+        return weather.toUpperCase()
+    }
+  }
+}
 
 const WeatherCard = (props) => {
+  let iconType = setIcon(props.icon)
 
-return (<div>Hello from the weather card</div>)
+  return (
+    <div>
+      <ReactAnimatedWeather className="weather-icon"
+        icon={iconType}
+        color={DEFAULTS.color}
+        size ={DEFAULTS.size}
+        animate={DEFAULTS.animate}
+      />
+      <p>{props.temp}</p>
+      <p>{props.summary}</p>
+      <p>{props.visibility}</p>
+    </div>
+  )
 
 }
 
--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -3,18 +3,20 @@ import '../App.css';
 import WeatherCard from '../components/WeatherCard.js'
 
 const PROXY = "https://cors-anywhere.herokuapp.com/"
-const API_KEY = "a95f35dc318f97d9e40f2bb8d422c171"
+const API_KEY = ""
 class WeatherContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
       latitude: "26.092889",
-      longitude: "85.948972"
+      longitude: "85.948972",
+      current_weather: {},
+      daily_weather: {}
     }
   }
 
   componentDidMount(){
-    fetch(`${PROXY}https://api.darksky.net/forecast/${API_KEY}/${this.state.latitude},${this.state.longitude}`)
+    fetch(`${PROXY}https://api.darksky.net/forecast/${API_KEY}/${this.state.latitude},${this.state.longitude}?exclude=[minutely,hourly]`)
       .then(response => {
         if (response.ok) {
           return response;
@@ -26,18 +28,22 @@ class WeatherContainer extends Component {
       })
       .then(response => response.json())
       .then(body => {
-        console.log(body)
-        debugger
-        return body
+        this.setState({current_weather: body.currently, daily_weather: body.daily})
       })
       .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
 
   render() {
+    debugger
     return (
       <div className="App">
-          <WeatherCard/>
+          <WeatherCard id="current-weather"
+            temp={this.state.current_weather.temperature}
+            visibility={this.state.current_weather.visibility}
+            summary={this.state.current_weather.summary}
+            icon = {this.state.current_weather.icon}
+          />
       </div>
     );
   }
